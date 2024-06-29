30b65ce5bc91316760b81792405f0320f4393794
atul <animeflix1r@gmail.com>
2024-06-29 14:38:41 +0530

    change apporoach

--- h.py
+++ h.py
@@ -1,1 +1,11 @@
-
+class employee():
+    name=""
+    id=""
+    time_period=""
+    def info(self):
+        print(f"Name:{self.name},ID:{self.id}\nThank you for {self.time_period} years, you have been with us .")   
+ankit=employee()
+ankit.name='ankit'
+ankit.id='2022084'
+ankit.time_period=2
+ankit.info()
